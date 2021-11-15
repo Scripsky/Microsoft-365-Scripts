44d524111c99dccb9c8b8f36eff3199d5d4a40f3
Mario Davis <50406492+Scripsky@users.noreply.github.com>
2021-11-15 15:09:02 -0600

    Add files via upload

---
+++ Get_License_From_Users.ps1
@@ -0,0 +1,36 @@
+﻿#install-module azuread
+#import-module azuread
+
+Connect-AzureAD
+
+#install-module msonline
+#import-module msonline
+
+Connect-MsolService
+
+
+$ids = import-csv -Path E:\id.csv
+
+foreach ($id in $ids){
+    Get-AzureADGroupMember -ObjectId $id.id | Out-File -FilePath E:\pmousers.csv -Append
+}
+
+$users = import-csv -Path E:\test.csv
+
+
+foreach ($user in $users){
+    $license = $null
+    $license = (Get-MsolUser -UserPrincipalName $user.upn).licenses | Where-Object -Property AccountSkuId -EQ GTLCORP:PROJECTPROFESSIONAL -ErrorAction SilentlyContinue
+    if ($license -ne $null){
+        $user.upn | out-file E:\project.csv -Append
+    }
+}
+
+foreach ($user in $users){
+    $license = $null
+    $license = (Get-MsolUser -UserPrincipalName $user.upn).licenses | Where-Object -Property AccountSkuId -EQ GTLCORP:VISIOCLIENT -ErrorAction SilentlyContinue
+    if ($license -ne $null){
+        $user.upn | out-file E:\visio.csv -Append
+    }
+}
+
---
+++ Low_License.ps1
@@ -0,0 +1,26 @@
+﻿$Mail        = 'user_management@gtl.net'
+$password    = Get-Content 'C:\Scripts\Credentials\M365_Encrypted_Password.txt' | ConvertTo-SecureString
+$credentials = New-Object System.Management.Automation.PsCredential($Mail, $password)
+$Helpdesk    = 'helpdesk@gtl.net'
+
+import-module msonline
+Connect-MsolService -Credential $credentials
+
+$e3ActiveUnits   = Get-MsolAccountSku | Where-Object -Property AccountSkuId -EQ GTLCORP:SPE_E3 | Select-Object -ExpandProperty ActiveUnits
+$f3ActiveUnits   = Get-MsolAccountSku | Where-Object -Property AccountSkuId -EQ GTLCORP:SPE_F1 | Select-Object -ExpandProperty ActiveUnits
+$e3ConsumedUnits = Get-MsolAccountSku | Where-Object -Property AccountSkuId -EQ GTLCORP:SPE_E3 | Select-Object -ExpandProperty ConsumedUnits
+$f3ConsumedUnits = Get-MsolAccountSku | Where-Object -Property AccountSkuId -EQ GTLCORP:SPE_F1 | Select-Object -ExpandProperty ConsumedUnits
+
+if (([int]$f3ConsumedUnits + 1) -ge [int]$f3ActiveUnits){
+   
+    Send-MailMessage -From 'User Management <user_management@gtl.net>' -To $Helpdesk `
+    -Subject 'User Backup Report' -SmtpServer 'smtp.office365.com' -Port 587 -UseSsl `
+    -Credential $Credentials -Body "There is only 1 F3 license left. Please order more or remove licenses from disabled users"
+}
+
+if (([int]$e3ConsumedUnits + 1) -ge [int]$e3ActiveUnits){
+    
+    Send-MailMessage -From 'User Management <user_management@gtl.net>' -To $Helpdesk `
+    -Subject 'User Backup Report' -SmtpServer 'smtp.office365.com' -Port 587 -UseSsl `
+    -Credential $Credentials -Body "There is only 1 E3 license left. Please order more or remove licenses from disabled users"
+}
