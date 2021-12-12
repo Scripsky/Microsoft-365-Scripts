2c9c9e60f7a687516b80868571d901fb574f393c
Mario Davis <50406492+Scripsky@users.noreply.github.com>
2021-12-12 14:01:00 -0600

    Update Get_License_From_Users.ps1

--- Get_License_From_Users.ps1
+++ Get_License_From_Users.ps1
@@ -5,7 +5,7 @@ Connect-MsolService
 $users = import-csv -Path "PATHTOCSV"
 
 foreach ($user in $users){
-    $license = (Get-MsolUser -UserPrincipalName $user.COLUMNNAME).licenses | Where-Object -Property AccountSkuId -EQ LICNSENAME -ErrorAction SilentlyContinue
+    $license = (Get-MsolUser -UserPrincipalName $user.COLUMNNAME).licenses | Where-Object -Property AccountSkuId -EQ LICENSENAME -ErrorAction SilentlyContinue
     if ($license -ne $null){
         $user.upn | out-file "PATHTOFILE" -Append
     }
